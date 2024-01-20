51852125be6cf071614459115c14553ca5ba95c7
Rhh <1466569866@qq.com>
2024-01-20 12:29:23 +0800

    init

---
+++ src/api/routes.ts
@@ -0,0 +1,12 @@
+// import { http } from "@/utils/http";
+
+// type Result = {
+//   success: boolean;
+//   data: Array<any>;
+// };
+
+export const getAsyncRoutes = async () => {
+  return {
+    data: []
+  };
+};
